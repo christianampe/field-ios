5484285833cd28e4a45c2cc8039f9a1d1934c8fa
christianampe <christianampe.work@gmail.com>
2018-07-29 16:57:36 -0700

    added files

---
+++ Example/Pods/Target Support Files/Pods-FlatField_Example/Pods-FlatField_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlatField_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FlatField_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-FlatField_Tests/Pods-FlatField_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlatField_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FlatField_Tests
+@end
